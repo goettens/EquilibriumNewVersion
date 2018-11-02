ce4a8c99295bae8c9e7f4010ae264a08d28f2bc8
Guilherme Goetten <goettens@hotmail.com>
2018-11-02 18:15:09 -0300

    Adiciona a tela de feed, view model respectiva e faz algumas alterações nos modelos das mesmas. Ainda não está fazendo a consulta no firebase e atualizando o feed

--- EquilibriumApp/EquilibriumApp/Models/Atividades.cs
+++ EquilibriumApp/EquilibriumApp/Models/Atividades.cs
@@ -4,19 +4,17 @@ using System.Text;
 
 namespace EquilibriumApp.Models
 {
-    class Atividades
+    public class Atividades
     {
         public int Id { get; set; }
-        public string Titulo { get; set; }
-        public string DescricaoCurta { get; set; }
-        public string DescricaoLonga { get; set; }
-        public float PrecoMinimo { get; set; }
-        public float PrecoMaximo { get; set; }
+        public string Name { get; set; }
+        public string LongDescription { get; set; }
+        public float MinimumPrice { get; set; }
+        public float MaximumPrice { get; set; }
         public string Link { get; set; }
-        public byte[] FotoCapa { get; set; }
-        public int EnumCategorias { get; set; }
+        public string ImageURL { get; set; }
+        public int EnumCategories { get; set; }
         public int Likes { get; set; }
-        public int IdCidade { get; set; }
         public int ReportsCount { get; set; }
     }
 }
---
+++ EquilibriumApp/EquilibriumApp/ViewModels/FeedViewModel.cs
@@ -0,0 +1,52 @@
+﻿using EquilibriumApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace EquilibriumApp.ViewModels
+{
+    public class FeedViewModel : BaseViewModel
+    {
+        public FeedViewModel()
+        {
+            Title = "Feed";
+            Atividades = new ObservableCollection<Atividades>();
+            AtividadesFiltradas = new ObservableCollection<Atividades>();
+            DetalharAtividadeCommand = new Command(async () => await DetalharAtividade());
+        }
+
+        public ObservableCollection<Atividades> Atividades { get; set; }
+        public ObservableCollection<Atividades> AtividadesFiltradas {get; set;}
+        public ICommand DetalharAtividadeCommand { get; set; }
+
+        private Atividades atividadeSelecionada;
+        public Atividades AtividadeSelecionada
+        {
+            get
+            {
+                return atividadeSelecionada;
+            }
+            set
+            {
+                SetProperty(ref atividadeSelecionada, value, nameof(AtividadeSelecionada));
+            }
+        }
+
+        public override Task InitializeAsync(object navigationData)
+        {
+
+
+            return base.InitializeAsync(navigationData);
+        }
+
+
+        private async Task DetalharAtividade()
+        {
+            await DialogService.ShowMessage("Em Desenvolvimento", "Aviso");
+        }
+    }
+}
